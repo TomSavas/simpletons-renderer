55bc001c820a111c8223ef64832fb8a209ee0d78
Savas <tomas.savickas@protonmail.com>
2020-05-27 08:41:55 +0100

    Fix big models producing segfaults, normalize coords

--- model.cpp
+++ model.cpp
@@ -7,6 +7,10 @@
 // Stolen directly from https://github.com/ssloy/tinyrenderer
 // Will redo in the nearest future
 Model::Model(const char *filepath) {
+    vertices = new std::vector<Vec3f>();
+    faces = new std::vector<std::vector<int>*>();
+    float max_value = 1;
+
     std::ifstream in;
     in.open (filepath, std::ifstream::in);
     if (in.fail()) return;
@@ -21,33 +25,54 @@ Model::Model(const char *filepath) {
             iss >> vertex.x;
             iss >> vertex.y;
             iss >> vertex.z;
-            vertices.push_back(vertex);
+
+            max_value = std::max(max_value, std::max(vertex.x, std::max(vertex.y, vertex.z)));
+
+            vertices->push_back(vertex);
         } else if (!line.compare(0, 2, "f ")) {
-            std::vector<int> f;
+            std::vector<int> *f = new std::vector<int>();
             int itrash, idx;
             iss >> trash;
             while (iss >> idx >> trash >> itrash >> trash >> itrash) {
                 idx--; // in wavefront obj all indices start at 1, not zero
-                f.push_back(idx);
+                f->push_back(idx);
             }
-            faces.push_back(f);
+            faces->push_back(f);
         }
     }
-    //std::cerr << "# v# " << vertices.size() << " f# "  << faces.size() << std::endl;
+
+    for (int i = 0; i < vertices->size(); i++) {
+        (*vertices)[i] = Vec3f((*vertices)[i].x / max_value + 1, 
+                (*vertices)[i].y / max_value + 1,
+                (*vertices)[i].z / max_value + 1);
+
+    }
+
+    std::cerr << "# v# " << vertices->size() << " f# "  << faces->size() << std::endl;
+}
+
+Model::~Model() {
+    free(vertices);
+
+    for (int i = 0; i < (*faces)[i]->size(); i++) {
+        free((*faces)[i]);
+    }
+
+    free(faces);
 }
 
 int Model::VertCount() const {
-    return vertices.size();
+    return vertices->size();
 }
 
 int Model::FaceCount() const {
-    return faces.size();
+    return faces->size();
 }
 
 Vec3f Model::Vert(int index) const {
-    return vertices[index];
+    return vertices->operator[](index);
 }
 
-std::vector<int> Model::Face(int index) const {
-    return faces[index];
+std::vector<int> *Model::Face(int index) const {
+    return faces->operator[](index);
 }
--- model.h
+++ model.h
@@ -7,17 +7,18 @@
 
 class Model {
 private:
-    std::vector<Vec3f> vertices;
-    std::vector<std::vector<int>> faces;
+    std::vector<Vec3f> *vertices;
+    std::vector<std::vector<int>*> *faces;
 
 public:
     Model(const char *filepath);
+    ~Model();
 
     int VertCount() const;
     int FaceCount() const;
 
     Vec3f Vert(int index) const;
-    std::vector<int> Face(int index) const;
+    std::vector<int> *Face(int index) const;
 };
 
 #endif
