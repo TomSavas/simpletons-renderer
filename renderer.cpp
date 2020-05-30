64ee704af3f600241d77c53998dbe7408c32a501
Savas <tomas.savickas@protonmail.com>
2020-05-30 18:03:25 +0100

    Add MVP transformations

--- renderer.cpp
+++ renderer.cpp
@@ -3,8 +3,9 @@
 #include <limits>
 
 #include "renderer.h"
+#include "matrix.h"
 
-Renderer::Renderer() : fb() {
+Renderer::Renderer(Mat4f projection_mat, Mat4f view_mat) : fb(), projection(projection_mat), view(view_mat) {
     z_buf = new std::vector<float>(fb.Width() * fb.Height(), -std::numeric_limits<float>::max());
 }
 
@@ -41,47 +42,19 @@ void Renderer::DrawLine(int x0, int y0, int x1, int y1, Color color) {
     }
 }
 
-void Renderer::DrawLine(Vec3f v0, Vec3f v1, Color color) { 
+void Renderer::DrawLine(Vec4f v0, Vec4f v1, Color color) { 
+    v0 = fb.ViewportMatrix() * (projection * view * v0).ProjectTo3d();
+    v1 = fb.ViewportMatrix() * (projection * view * v1).ProjectTo3d();
+
     DrawLine(v0.X(), v0.Y(), v1.X(), v1.Y(), color);
 }
 
-void Renderer::DrawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f uv0, Vec3f uv1, Vec3f uv2, Color color) {
-    float min_x = std::min(v0.X(), std::min(v1.X(), v2.X()));
-    float min_y = std::min(v0.Y(), std::min(v1.Y(), v2.Y()));
-
-    float max_x = std::max(v0.X(), std::max(v1.X(), v2.X()));
-    float max_y = std::max(v0.Y(), std::max(v1.Y(), v2.Y()));
-
-    // Could do binary search on x to see where the triangle starts if the image is bigger than say 64?
-    for (int y = min_y; y <= max_y; y++) {
-        bool drawing_triangle = false;
-
-        for (int x = min_x; x <= max_x; x++) {
-            Vec3f point(x, y);
-            Vec3f barycentric = point.Barycentric(v0, v1, v2);
-
-            if (barycentric.X() < 0 || barycentric.Y() < 0 || barycentric.Z() < 0) {
-                // If we are already drawing a triangle and we stepped out of it
-                // we won't be drawing it again on this line.
-                if (drawing_triangle)
-                    break;
-
-                continue;
-            }
-            drawing_triangle = true;
-
-            float point_z = barycentric.X() * v0.Z() + barycentric.Y() * v1.Z() + barycentric.Z() * v2.Z();
-            int z_buf_index = ZBufIndex(x, y);
-            if (point.Z() > z_buf->operator[](z_buf_index)) {
-                z_buf->operator[](z_buf_index) = point_z;
-                fb.Set(x, y, color);
-            }
-        }
-    }
-}
-
-void Renderer::DrawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f uv0, Vec3f uv1, Vec3f uv2,
+void Renderer::DrawTriangle(Vec4f v0, Vec4f v1, Vec4f v2, Vec3f uv0, Vec3f uv1, Vec3f uv2,
         float lighting_intensity, TGAImage &tex) {
+    v0 = fb.ViewportMatrix() * v0;
+    v1 = fb.ViewportMatrix() * v1;
+    v2 = fb.ViewportMatrix() * v2;
+
     float min_x = std::min(v0.X(), std::min(v1.X(), v2.X()));
     float min_y = std::min(v0.Y(), std::min(v1.Y(), v2.Y()));
 
@@ -90,11 +63,19 @@ void Renderer::DrawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f uv0, Vec3f uv1, 
 
     // Could do binary search on x to see where the triangle starts if the image is bigger than say 64?
     for (int y = min_y; y <= max_y; y++) {
+        // Ignore the point if it's out of viewport's bounds
+        if (y < 0 || y >= fb.Height())
+            continue;
+
         bool drawing_triangle = false;
 
         for (int x = min_x; x <= max_x; x++) {
+            // Ignore the point if it's out of viewport's bounds
+            if (x < 0 || x >= fb.Width())
+                continue;
+
             Vec3f point(x, y);
-            Vec3f barycentric = point.Barycentric(v0, v1, v2);
+            Vec3f barycentric = point.Barycentric(v0.To<Vec3f, 3>(), v1.To<Vec3f, 3>(), v2.To<Vec3f, 3>());
 
             if (barycentric.X() < 0 || barycentric.Y() < 0 || barycentric.Z() < 0) {
                 // If we are already drawing a triangle and we stepped out of it
@@ -106,13 +87,15 @@ void Renderer::DrawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f uv0, Vec3f uv1, 
             }
             drawing_triangle = true;
 
-            float point_z = barycentric.X() * v0.Z() + barycentric.Y() * v1.Z() + barycentric.Z() * v2.Z();
-            float u = (barycentric.X() * uv0.X() + barycentric.Y() * uv1.X() + barycentric.Z() * uv2.X()) * tex.get_width();
-            float v = (barycentric.X() * uv0.Y() + barycentric.Y() * uv1.Y() + barycentric.Z() * uv2.Y()) * tex.get_height();
-            TGAColor tga_color = tex.get(u, v) * lighting_intensity;
+            float u = (barycentric.X() * uv0.X() + barycentric.Y() * uv1.X() + barycentric.Z() * uv2.X()) * 
+                tex.get_width();
+            float v = (barycentric.X() * uv0.Y() + barycentric.Y() * uv1.Y() + barycentric.Z() * uv2.Y()) *
+                tex.get_height();
 
+            TGAColor tga_color = tex.get(u, v) * lighting_intensity;
             Color color(tga_color);
 
+            float point_z = barycentric.X() * v0.Z() + barycentric.Y() * v1.Z() + barycentric.Z() * v2.Z();
             int z_buf_index = ZBufIndex(x, y);
             if (point.Z() > z_buf->operator[](z_buf_index)) {
                 z_buf->operator[](z_buf_index) = point_z;
@@ -122,29 +105,38 @@ void Renderer::DrawTriangle(Vec3f v0, Vec3f v1, Vec3f v2, Vec3f uv0, Vec3f uv1, 
     }
 }
 
-void Renderer::DrawModel(const Model &model, TGAImage &tex, Vec3f insertion_point, float scale) {
-    Vec3f offset(400, 50, 0);
+void Renderer::DrawModel(const Model &model, TGAImage &tex, Mat4f model_mat) {
+    Mat4f mvp = projection * view * model_mat;
     Vec3f light_dir(0, 0, -1);
 
     for (int i = 0; i < model.FaceCount(); i++) {
         std::vector<FaceIndices> *face = model.Face(i);
-        Vec3f v0 = ((Vec3f(1, 1, 1) + model.Vert(face->operator[](0).vertex_index)) * scale)
-            .TruncateCoeffs() + insertion_point;
-        Vec3f v1 = ((Vec3f(1, 1, 1) + model.Vert(face->operator[](1).vertex_index)) * scale)
-            .TruncateCoeffs() + insertion_point;
-        Vec3f v2 = ((Vec3f(1, 1, 1) + model.Vert(face->operator[](2).vertex_index)) * scale)
-            .TruncateCoeffs() + insertion_point;
+
+        Vec4f v0 = (model.Vert(face->operator[](0).vertex_index));
+        Vec4f v1 = (model.Vert(face->operator[](1).vertex_index));
+        Vec4f v2 = (model.Vert(face->operator[](2).vertex_index));
+
+        v0 = (mvp * v0).ProjectTo3d();
+        v1 = (mvp * v1).ProjectTo3d();
+        v2 = (mvp * v2).ProjectTo3d();
 
         Vec3f uv0 = model.Uv(face->operator[](0).uv_index);
         Vec3f uv1 = model.Uv(face->operator[](1).uv_index);
         Vec3f uv2 = model.Uv(face->operator[](2).uv_index);
 
         // Simple lighting
-        Vec3f triangle_normal = (v2 - v0).Cross(v1 - v0).Normalize();
+        Vec3f triangle_normal = (v2.To<Vec3f, 3>() - v0.To<Vec3f, 3>())
+            .Cross(v1.To<Vec3f, 3>() - v0.To<Vec3f, 3>()).Normalize();
         float lighting_intensity = triangle_normal.Dot(light_dir);
         if (lighting_intensity < 0) 
             continue;
 
         DrawTriangle(v0, v1, v2, uv0, uv1, uv2, lighting_intensity, tex);
     }
+}
+
+void Renderer::DrawAxes() {
+    DrawLine(Vec4f(0, 0, 0, 1), Vec4f(0.25, 0, 0, 1), Color(255, 0, 0, 0));
+    DrawLine(Vec4f(0, 0, 0, 1), Vec4f(0, 0.25, 0, 1), Color(0, 255, 0, 0));
+    DrawLine(Vec4f(0, 0, 0, 1), Vec4f(0, 0, 0.25, 1), Color(0, 0, 255, 0));
 }
--- renderer.h
+++ renderer.h
@@ -6,24 +6,31 @@
 #include "color.h"
 #include "model.h"
 #include "tgaimage.h"
+#include "matrix.h"
 
 class Renderer {
 private:
     Framebuf fb;
     std::vector<float> *z_buf;
+    Mat4f projection;
+    Mat4f view;
 
     int ZBufIndex(int x, int y) const;
 
     void DrawLine(int x0, int y0, int x1, int y1, Color color);
 
 public:
-    Renderer();
+    Renderer(Mat4f projection_mat, Mat4f view_mat);
 
-    void DrawLine(Vec3f line_start, Vec3f line_end, Color color);
-    void DrawTriangle(Vec3f t0, Vec3f t1, Vec3f t2, Vec3f uv0, Vec3f uv1, Vec3f uv2, Color color);
-    void DrawTriangle(Vec3f t0, Vec3f t1, Vec3f t2, Vec3f uv0, Vec3f uv1, Vec3f uv2,
+    void DrawLine(Vec4f line_start, Vec4f line_end, Color color);
+    void DrawTriangle(Vec4f t0, Vec4f t1, Vec4f t2, Vec3f uv0, Vec3f uv1, Vec3f uv2,
             float lighting_intensity, TGAImage &tex);
-    void DrawModel(const Model &model, TGAImage &tex, Vec3f insertion_point, float scale);
+    void DrawModel(const Model &model, TGAImage &tex, Mat4f model_mat);
+
+    void DrawAxes();
+
+    void SetProjection(Mat4f projection_mat);
+    void SetView(Mat4f view_mat);
 };
 
 #endif
