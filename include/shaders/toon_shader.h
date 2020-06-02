3c58b41a555289b800470af9f2688964baac7e36
Savas <tomas.savickas@protonmail.com>
2020-06-02 21:54:54 +0100

    Add toon shader

---
+++ include/shaders/toon_shader.h
@@ -0,0 +1,18 @@
+#ifndef TOON_SHADER_H
+#define TOON_SHADER_H
+
+#include "shaders/gouraud_shader.h"
+
+class ToonShader : public GouraudShader {
+protected:
+    Color color;
+    int shade_count;
+    float intensity_per_shade;
+public:
+    ToonShader(Vec3f light_dir, Color color, int shade_count);
+
+    virtual std::tuple<bool, Color> Fragment(const Vec3f &barycentric,
+        const FaceInfo &face, TGAImage &tex) override;
+};
+
+#endif
---
+++ src/shaders/toon_shader.cpp
@@ -0,0 +1,30 @@
+#include <tuple>
+
+#include "shaders/toon_shader.h"
+
+ToonShader::ToonShader(Vec3f light_dir, Color color, int shade_count) :
+    GouraudShader(light_dir), color(color), shade_count(shade_count) {
+    intensity_per_shade = 1.0 / (shade_count - 1);
+}
+
+std::tuple<bool, Color> ToonShader::Fragment(const Vec3f &barycentric, const FaceInfo &face,
+    TGAImage &tex) {
+    if (barycentric.X() < 0 || barycentric.Y() < 0 || barycentric.Z() < 0)
+        return std::make_tuple(false, Color(0, 0, 0, 0));
+
+    float intensity = intensities.Dot(barycentric);
+    bool zero_intensity = true;
+
+    for (int i = shade_count; i > 0; i--) {
+        if (intensity > intensity_per_shade * i) {
+            intensity = intensity_per_shade * i;
+            zero_intensity = false;
+            break;
+        }
+    }
+
+    if (zero_intensity)
+        intensity = 0;
+
+    return std::make_tuple(true, color * intensity);
+}
